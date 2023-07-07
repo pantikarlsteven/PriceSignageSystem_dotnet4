96bc1599dd475865d3b161291d69f78a1f52099b
noellangcauon <langcauonoel@gmail.com>
2023-07-07 15:59:39 +0800

    migration changes

---
+++ PriceSignageSystem/Migrations/202307070755032_add_InventoryPrintingLogTbl.cs
@@ -0,0 +1,28 @@
+﻿namespace PriceSignageSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_InventoryPrintingLogTbl : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.InventoryPrintingLogs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        O3SKU = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        PrintedBy = c.String(),
+                        DateCreated = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.InventoryPrintingLogs");
+        }
+    }
+}
