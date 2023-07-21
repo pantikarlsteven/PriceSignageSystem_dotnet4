42fb74582eedc611ddf17cd550fb2dacfdecbfbb
noellangcauon <langcauonoel@gmail.com>
2023-07-21 11:11:55 +0800

    Adding PriceSignageAutoPrintingService to print "with inventory" pcas and "sku changes" adjusting whole report model field

---
+++ PriceSignageSystem/Models/Dto/AutoPrintStatusDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PriceSignageSystem.Models.Dto
+{
+    public class AutoPrintStatusDto
+    {
+        public bool IsPrinted { get; set; }
+        public decimal O3DATE { get; set; }
+    }
+}
